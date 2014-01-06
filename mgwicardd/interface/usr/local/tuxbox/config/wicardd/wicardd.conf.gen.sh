35cd44291b9f5ee1df2f38ebad39dd8057cafc01
juri775@narod.ru <juri775@narod.ru>
2014-01-06 19:35:13 +0200

    Signed-off-by: juri775@narod.ru <juri775@narod.ru>

--- mgwicardd/interface/usr/local/tuxbox/config/wicardd/wicardd.conf.gen.sh
+++ mgwicardd/interface/usr/local/tuxbox/config/wicardd/wicardd.conf.gen.sh
@@ -1,21 +1,10 @@
 #!/bin/sh
 set -x
-FILE="/home/root/.xbmc/userdata/addon_data/plugin.program.MgWicardd"
+#FILE="/home/root/.xbmc/userdata/addon_data/plugin.program.MgWicardd"
+CONFIG="./wicardd.conf.gen.conf"
+FILE=`pwd`
 #####Server 1
-E="_enable"
-enable=`grep $SRV$E $FILE/settings.xml | grep -o value.* | sed 's/value="//g' | sed 's/" \/>//g' | sed 's/true/1/g' | sed 's/false/0/'`
-L="_login"
-login=`grep $SRV$L $FILE/settings.xml | grep -o value.* | sed 's/value="//g' | sed 's/" \/>//g'`
-N="_name"
-name=`grep $SRV$N $FILE/settings.xml | grep -o value.* | sed 's/value="//g' | sed 's/" \/>//g'`
-P="_pass"
-pass=`grep $SRV$P $FILE/settings.xml | grep -o value.* | sed 's/value="//g' | sed 's/" \/>//g'`
-PO="_port"
-port=`grep $SRV$PO $FILE/settings.xml | grep -o value.* | sed 's/value="//g' | sed 's/" \/>//g'`
-S="_server"
-server=`grep $SRV$S $FILE/settings.xml | grep -o value.* | sed 's/value="//g' | sed 's/" \/>//g'`
-T="_type"
-type=`grep $SRV$T $FILE/settings.xml | grep -o value.* | sed 's/value="//g' | sed 's/" \/>//g'`
+
 #####Server 1
 
 cat <<EOF
@@ -24,14 +13,14 @@ active = 1
 #log = /tmp/w2.log 
 log_size = 100
 debug = 3                    
-daemon = 1 "
+daemon = 1
 
 EOF
 cat <<EOF
 [webif]
 active = 0                        
 port = 8081                    
-debug = 1"
+debug = 1
 
 EOF
 cat <<EOF
@@ -64,8 +53,27 @@ debug = 1
 
 EOF
 #Server 1
-SRV=sdfdsdshsrd1
+SRV=srv1
+. "${CONFIG}"
+
 echo "######################$SRV$E#########    $enable##############"
+cat <<EOF
+[reader]
+active = $enable
+name = $name
+type = $type
+async_mode = 1
+account = $login:$pass@$server:$port
+reconnect_delay = 10
+debug = 1
+emm_cache = 1 
+ecm_ttl = 5000"
+
+EOF
+
+SRV=srv2
+. "${CONFIG}"
+
 cat <<EOF
 [reader]
 active = $enable
@@ -80,8 +88,60 @@ ecm_ttl = 5000
 
 EOF
 
-SRV=srv2
-echo "######################$SRV$E#########    $enable##############"
+SRV=srv3
+. "${CONFIG}"
+
+cat <<EOF
+[reader]
+active = $enable
+name = $name
+type = $type
+async_mode = 1
+account = $login:$pass@$server:$port
+reconnect_delay = 10
+debug = 1
+emm_cache = 1 
+ecm_ttl = 5000
+
+EOF
+
+SRV=srv4
+. "${CONFIG}"
+
+cat <<EOF
+[reader]
+active = $enable
+name = $name
+type = $type
+async_mode = 1
+account = $login:$pass@$server:$port
+reconnect_delay = 10
+debug = 1
+emm_cache = 1 
+ecm_ttl = 5000
+
+EOF
+
+SRV=srv5
+. "${CONFIG}"
+
+cat <<EOF
+[reader]
+active = $enable
+name = $name
+type = $type
+async_mode = 1
+account = $login:$pass@$server:$port
+reconnect_delay = 10
+debug = 1
+emm_cache = 1 
+ecm_ttl = 5000
+
+EOF
+
+SRV=srv6
+. "${CONFIG}"
+
 cat <<EOF
 [reader]
 active = $enable
